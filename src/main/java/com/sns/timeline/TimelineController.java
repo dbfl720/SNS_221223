64fc34755f831ee604921590a68bb005c59fb529
YURI HONG <dbfl720@gmail.com>
2023-04-18 19:49:49 +0900

    d

--- src/main/java/com/sns/timeline/TimelineController.java
+++ src/main/java/com/sns/timeline/TimelineController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpSession;
+
 @RequestMapping("/timeline")
 @Controller
 public class TimelineController {
@@ -12,7 +14,15 @@ public class TimelineController {
 	
 	//localhost:8080/timeline/timeline_view
 	@GetMapping("/timeline_view")
-	public String timelineView(Model model) {
+	public String timelineView(Model model, HttpSession session) {
+		
+		Integer userId = (Integer)session.getAttribute("userId");
+		
+		
+		// select DB
+		List<Post> postList = 
+		
+		model.addAttribute("postList", postList);
 		model.addAttribute("view", "timeline/timeline");  // jsp - timeline폴더 - timeline.jsp 
 		
 		return "template/layout";
